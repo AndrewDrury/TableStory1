2c1f2349b377435ae6df1dfc77f44129d7cce1b5
Andy Wang <sam.andy.wang@gmail.com>
2019-02-16 15:58:13 -0500

    webcam is showing on side of interaction view

--- src/views/Interaction.vue
+++ src/views/Interaction.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="interaction">
     <h1>This is the interaction page</h1>
+    <video autoplay id="webcam" class="d-flex align-items-start"></video>
   </div>
 </template>
 
@@ -8,3 +9,52 @@
 <style lang="scss">
 @import url("/src/assets/main.scss");
 </style>
+
+<script>
+export default {
+  data: () => {
+    return {}
+  },
+  mounted: () => {
+    navigator.mediaDevices.enumerateDevices().then( devices =>
+		{
+      console.log(devices)
+      // this is the device id of webcam. WILL NEED TO CHANGE THIS FOR THE LOGITECH WEBCAM
+			devices= devices.filter( v => (v.deviceId == "ccc2ae6220fa0a931628143b6d253ae6fda4906cb3c601c4ff2880392935907a")); 
+			let device= devices[0];
+			if( !device )
+			{
+				console.log("No devices!");
+				return;
+			} else {
+        console.log(device)
+      }
+			
+			let constraints =
+			{
+				audio: false, 
+				video: {
+					deviceId: { ideal: device.deviceId },
+					width: { ideal: window.innerWidth/3 },
+					height: { ideal: window.innerHeight }
+				}
+			};
+			navigator.mediaDevices.getUserMedia(constraints)
+			.then( stream =>
+			{
+        const video = document.getElementById("webcam")
+        video.srcObject = stream
+				console.log("DONE");
+			})
+			.catch( err =>
+			{
+				console.log(err.name + ": " + err.message);
+			});	
+		})
+		.catch( err =>
+		{
+			console.log(err.name + ": " + err.message);
+		});
+  }
+}
+</script>
